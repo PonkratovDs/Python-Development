28ae1dab0d7bdff015918cacba40d300e38b0c18
Ponkratov Danil <ponkratov.ds17@physics.msu.ru>
2020-01-28 21:32:15 +0300

    real_path update

--- realPath/real_path.py
+++ realPath/real_path.py
@@ -31,7 +31,14 @@ class DLinkedList:
         self.NIL.prev = new_node
 
     def delete(self, del_node):
-        
+        curr_node = self.NIL.prev    #т.к. более вероятно, что с конца будет элемент в реализации ниже
+        while curr_node != self.NIL:
+            if curr_node == del_node:
+                del_node.prev.follow = del_node.follow
+                del_node.follow.prev = del_node.prev
+                del del_node
+                return
+            curr_node = curr_node.prev
 
     def _back_list_crowl(self):
         curr_node = self.NIL.prev
@@ -39,7 +46,32 @@ class DLinkedList:
             print(curr_node.key)
             curr_node = curr_node.prev
 
+    def __str__(self):
+        super().__str__()
+        str_ = ''
+        curr_node = self.NIL.prev
+        while curr_node != self.NIL:
+            str_ += str(curr_node.key)
+            curr_node = curr_node.prev
+        return str_
+
+
+class RealPath:
+
+    def __init__(self, path):
+        super().__init__()
+        self.user_path = path
+        self._DLL = DLinkedList()
+
+    def _fill_DLinkedList(self):
+        pass
+
+
+
+
 DLL = DLinkedList()
 for i in range(1, 101):
     DLL.insert(i)
+DLL.delete(DLL.NIL.prev.prev)
 DLL._back_list_crowl()
+print(DLL)
